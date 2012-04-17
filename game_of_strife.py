5cba386c239d88f37b539e3036e2d40792841444
Yuval Langer <yuval.langer@gmail.com>
2012-04-17 08:43:57 +0300

    Non-working object orientation

--- game_of_strife.py
+++ game_of_strife.py
@@ -4,223 +4,218 @@ import scipy as sp
 import scipy.signal
 import pygame
 
-# TODO: How do I figure out what's the wanted frequency of mutation and diffusion?
-# TODO: Where exactly do I "global foo"? In which scopes and namespaces does "foo" lives?
-# BUG: check for sameness shouldn't be on its own loop but just at the top of the algorithm's loop so each check will be followed.immediately by one diffuse call and one mutate call.
-# 
+# TODO: How do I figure out what's the wanted frequency of mutation and
+#       diffusion?
+# TODO: Where exactly do I "global foo"? In which scopes and namespaces does
+#       "foo" lives?
+# TODO: BUG: check for sameness shouldn't be on its own loop but just at the
+#       top of the algorithm's loop so each check will be followed.immediately
+#       by one diffuse call and one mutate call.
+# TODO: Getting this into OO
 
-## functions
+class gos:
+    def __init__(self):
+        ## settings
+        
+        # Board size
+        self.N = 50
+        self.cell_num = self.N**2
+        
+        ## time keeping
+        # number of generations the simulation will run
+        # each generation is defined as the average number of steps for which each cell
+        # on the board was in a competition once since last generation (?)
 
-def competiroll(N):
-    """draw two competitor positions"""
-    # We'll use relative positions to compute exact positions of 2nd competitor cell
-    NEIGHBOUR_ROW = sp.array([-1,  0,  1, -1,  0,  1, -1,  1])
-    NEIGHBOUR_COL = sp.array([-1, -1, -1,  1,  1,  1,  0,  0])
-    NEIGHBOUR_REL_POS = sp.array(zip(NEIGHBOUR_ROW, NEIGHBOUR_COL))
-    c1 = sp.random.randint(N, size=2)
-    c2 = c1 + NEIGHBOUR_REL_POS[sp.random.randint(8, size=1)[0]]
-    return c1, c2
+        # we'll increase this by one every time two cells compete.
+        self.step = 0
 
+        self.generations = 50
+        self.steps_final = self.generations * self.cell_num
 
-## settings
+        # number of genotypes possible
+        self.genotype_num = 8
 
-# Board size
-N = 50
-cell_num = N**2
+        # Cost of gene expression
 
-## time keeping
-# number of generations the simulation will run
-# each generation is defined as the average number of steps for which each cell
-# on the board was in a competition once since last generation (?)
+        self.S_cost = 3
+        self.R_cost = 8
+        self.C_cost = 30
 
-# we'll increase this by one every time two cells compete.
-step = 0
+        # cooperation benefit, in ratio
+        self.benefit = 0.3
+        
+        # radius
+        self.S_rad = 1
+        self.C_rad = 1
 
-generations = 50
-steps_final = generations * cell_num
+        # diameter of the convolution matrix
+        diameter = lambda x: 2 * x + 1
+        self.S_len = diameter(self.S_rad)
+        self.C_len = diameter(self.C_rad)
 
-# number of genotypes possible
-genotype_num = 8
+        # the convolution matrix used to count neighbours
+        self.S_kernel = sp.ones((self.S_len, self.S_len))
+        self.C_kernel = sp.ones((self.C_len, self.C_len)) # convolution matrix used to count cells that produce public goods
 
-# Cost of gene expression
+        # neighbours effects' thresholds
+        self.S_th = 3 # quorum threshold
+        self.C_th = 3 # Cooperation threshold. Above it, public goods makes a difference.
 
-S_cost = 3
-R_cost = 8
-C_cost = 30
+        # A cell can be Signalling and/or Receptive and/or Cooperative
+        self.S = sp.rand(self.N, self.N) < 0.5
+        self.R = sp.rand(self.N, self.N) < 0.5
+        self.C = sp.rand(self.N, self.N) < 0.5
 
-# cooperation benefit, in ratio
-benefit = 0.3
+        ## data sampling
+        # we will take a frequency sample some number of times per generation
+        self.steps_per_gen = self.N**2
+        self.samples_per_gen = 100
+        self.samples_num = self.samples_per_gen * self.generations
+        self.steps_per_sample = sp.uint32(sp.floor(1.0 * self.steps_per_gen / self.samples_per_gen))
+        self.sample_count = 0
+        # We want to know the frequency of each genotype per generation
+        self.samples_frequency = sp.empty((self.samples_num, self.genotype_num))
+        self.samples_nhood = sp.empty((self.samples_num, self.genotype_num, self.genotype_num))
 
-# radius
-S_rad = 1
-C_rad = 1
+        # pygame initialization
 
-# diameter of the convolution matrix
-diameter = lambda x: 2 * x + 1
-S_len = diameter(S_rad)
-C_len = diameter(C_rad)
+        #pygame.init()
+        #screen = pygame.display.set_mode((N*4, N*4))
+        #pygame.display.set_caption("lets see")
 
-# the convolution matrix used to count neighbours
-S_counter = sp.ones((S_len, S_len))
-C_counter = sp.ones((C_len, C_len)) # convolution matrix used to count cells that produce public goods
+    ## functions
 
-# neighbours effects' thresholds
-S_th = 3 # quorum threshold
-C_th = 3 # Cooperation threshold. Above it, public goods makes a difference.
+    def competiroll(N):
+        #"""draw two competitor positions stuff"""
+        # We'll use relative positions to compute exact positions of 2nd competitor cell
+        NEIGHBOUR_ROW = sp.array([-1,  0,  1, -1,  0,  1, -1,  1])
+        NEIGHBOUR_COL = sp.array([-1, -1, -1,  1,  1,  1,  0,  0])
+        NEIGHBOUR_REL_POS = sp.array(zip(NEIGHBOUR_ROW, NEIGHBOUR_COL))
+        c1 = sp.random.randint(N, size=2)
+        c2 = c1 + NEIGHBOUR_REL_POS[sp.random.randint(8, size=1)[0]]
+        return c1, c2
 
-# A cell can be Signalling and/or Receptive and/or Cooperative
-S = sp.rand(N, N) < 0.5
-R = sp.rand(N, N) < 0.5
-C = sp.rand(N, N) < 0.5
+    def competition(self):
+        global C, R, S, step
+        # compete
+        competitor_1, competitor_2 = self.competiroll(self.N)
+        # competitor_2's coordinates in a torus:
+        competitor_2t = competitor_2 % self.N
+        # we'll run this until we get a pair of competitors that are actually different:
+        while ((self.R[competitor_1[0], competitor_1[1]] == self.R[competitor_2t[0], competitor_2t[1]]) and
+               (self.S[competitor_1[0], competitor_1[1]] == self.S[competitor_2t[0], competitor_2t[1]]) and
+               (self.C[competitor_1[0], competitor_1[1]] == self.C[competitor_2t[0], competitor_2t[1]])):
+            competitor_1, competitor_2 = self.competiroll(self.N)
+            competitor_2t = competitor_2 % self.N
+            # time passes:
+            self.step += 1
+        # print "competitor_1, competitor_2"
+        # print competitor_1, competitor_2
+        # here we produce torusified versions of the boards.
+        # for signallers, we take both S_rad and self.C_rad around our competitors because,
+        # signallers affect receptive && cooperating cells which affect our competitors
+        S_sub = self.S[sp.arange(- self.S_rad - self.C_rad, self.S_rad + self.C_rad + 1)%self.N, :][:, sp.arange(- self.S_rad - self.C_rad, self.S_rad + self.C_rad + 1)%self.N]
+        R_sub = self.R[sp.arange(- self.C_rad, self.C_rad + 1)%self.N, :][:, sp.arange(- self.C_rad, self.C_rad + 1)%self.N]
+        C_sub = self.C[sp.arange(- self.C_rad, self.C_rad + 1)%self.N, :][:, sp.arange(- self.C_rad, self.C_rad + 1)%self.N]
+        #    print "S_sub.shape, R_sub.shape, C_sub.shape"
+        #    print S_sub.shape, R_sub.shape, C_sub.shape
+        # we count how many signallers are within each cell's neighbourhood
+        S_conv = sp.signal.convolve2d(S_sub, self.S_kernel, mode='valid')
+        # a cell will produce common goods if it's receptive and cooperative and signal in its neighborhood is above threshold
+        cooping_cells = (C_sub == R_sub) == (S_conv > self.S_th)
+        # how many cooperators around each competitor?
+        #    print "cooping_cells"
+        #    print cooping_cells.shape
+        #    print cooping_cells
+        C_conv = sp.signal.convolve2d(cooping_cells, self.C_kernel, mode='valid')
+        # Public goods effect.
+        # G for Goods
+        G = (C_conv > self.C_th)
+        #    print "G.shape", G.shape
+        # all cells for which the effect of goods is above threshold is True in G.
+        # M for Metabolism
+        cost_board = self.S_cost * self.S + self.R_cost * self.R + self.C_cost * self.C
+        M = G * (1 - self.benefit) * cost_board
+        # all false in G don't benefit from public goods (G^True flips values)
+        M += (G^True) *  cost_board
+        if self.M[competitor_1[0], competitor_1[1]] > self.M[competitor_2t[0], competitor_2t[1]]:
+            self.C[competitor_1[0], competitor_1[1]] = self.C[competitor_2t[0], competitor_2t[1]]
+            self.S[competitor_1[0], competitor_1[1]] = self.S[competitor_2t[0], competitor_2t[1]]
+            self.R[competitor_1[0], competitor_1[1]] = self.R[competitor_2t[0], competitor_2t[1]]
+#        elif M[competitor_1[0], competitor_1[1]] == M[competitor_2t[0], competitor_2t[1]]:
+#            nothing #print 'buga'
+        else:
+            self.C[competitor_2t[0], competitor_2t[1]] = self.C[competitor_1[0], competitor_1[1]]
+            self.S[competitor_2t[0], competitor_2t[1]] = self.S[competitor_1[0], competitor_1[1]]
+            self.R[competitor_2t[0], competitor_2t[1]] = self.R[competitor_1[0], competitor_1[1]]    
 
-## data sampling
-# we will take a frequency sample some number of times per generation
-steps_per_gen = N**2
-samples_per_gen = 100
-samples_num = samples_per_gen * generations
-steps_per_sample = sp.uint32(sp.floor(1.0 * steps_per_gen / samples_per_gen))
-sample_count = 0
-# We want to know the frequency of each genotype per generation
-samples_frequency = sp.empty((samples_num, genotype_num))
-samples_nhood = sp.empty((samples_num, genotype_num, genotype_num))
+    def mutate(self):
+        if sp.random.random()>0.1:
+            coords = sp.random.randint(self.N, size=2)
+            B = [self.C, self.R, self.S][sp.random.randint(3)]
+            B[coords[0], coords[1]] = sp.random.randint(2)
 
-## main stuff
+    def diffuse(self):
+        m, n = sp.random.randint(self.N, size=2)
+        m1, n1 = (m+1)%self.N, (n+1)%self.N
+        if sp.random.randint(2):
+            # Truth for clockwise
+            for board in [self.R, self.S, self.C]:
+                board[[m, m, m1, m1], [n, n1, n1, n]] = board[[m1, m, m, m1], [n, n, n1, n1]]
+        else:
+            for board in [self.R, self.S, self.C]:
+                board[[m, m, m1, m1], [n, n1, n1, n]] = board[[m, m1, m1, m], [n1, n1, n, n]]
 
-# pygame initialization
+    def sample(self):
+        global S, R, C, sample_count, samples_frequency, samples_nhood
+        for genotype in sp.arange(8):
+            genotype_board = (sp.bool8((not 1 & genotype) ^ self.S) +
+                              sp.bool8((not 2 & genotype) ^ self.R) +
+                              sp.bool8((not 4 & genotype) ^ self.C))
+            for j in sp.arange(8):
+                genotype_board
+                self.sample_nhood[self.sample_count, genotype]
+            self.sample_frequency[self.sample_count, genotype] = sum(genotype_board)
+        print sum(self.sample_frequency[self.sample_count])
+        self.sample_count += 1
+                
+    def mainstuff(self):
+        global while_count, step, data
+        #print "while_count", while_count
+        self.competition()
+        self.diffuse()
+        if not self.step % self.steps_per_sample: self.sample()
+        while_count += 1
+        print self.step
 
-#pygame.init()
-#screen = pygame.display.set_mode((N*4, N*4))
-#pygame.display.set_caption("lets see")
+    ## process data
 
-def competition():
-    global C, R, S, step
-    ## compete
-    competitor_1, competitor_2 = competiroll(N)
-    # competitor_2's coordinates in a torus:
-    competitor_2t = competitor_2 % N
-    # we'll run this until we get a pair of competitors that are actually different:
-    while ((R[competitor_1[0], competitor_1[1]] == R[competitor_2t[0], competitor_2t[1]]) and
-           (S[competitor_1[0], competitor_1[1]] == S[competitor_2t[0], competitor_2t[1]]) and
-           (C[competitor_1[0], competitor_1[1]] == C[competitor_2t[0], competitor_2t[1]])):
-        competitor_1, competitor_2 = competiroll(N)
-        competitor_2t = competitor_2 % N
-        # time passes:
-        step += 1
-#    print "competitor_1, competitor_2"
-#    print competitor_1, competitor_2
-    # here we produce torusified versions of the boards.
-    # for signallers, we take both S_rad and C_rad around our competitors because,
-    # signallers affect receptive && cooperating cells which affect our competitors
-    S_sub = S[sp.arange(- S_rad - C_rad, S_rad + C_rad + 1)%N, :][:, sp.arange(- S_rad - C_rad, S_rad + C_rad + 1)%N]
-    R_sub = R[sp.arange(- C_rad, C_rad + 1)%N, :][:, sp.arange(- C_rad, C_rad + 1)%N]
-    C_sub = C[sp.arange(- C_rad, C_rad + 1)%N, :][:, sp.arange(- C_rad, C_rad + 1)%N]
-#    print "S_sub.shape, R_sub.shape, C_sub.shape"
-#    print S_sub.shape, R_sub.shape, C_sub.shape
-    # we count how many signallers are within each cell's neighbourhood
-    S_conv = sp.signal.convolve2d(S_sub, S_counter, mode='valid')
-    # a cell will produce common goods if it's receptive and cooperative and signal in its neighborhood is above threshold
-    cooping_cells = (C_sub == R_sub) == (S_conv > S_th)
-    # how many cooperators around each competitor?
-#    print "cooping_cells"
-#    print cooping_cells.shape
-#    print cooping_cells
-    C_conv = sp.signal.convolve2d(cooping_cells, C_counter, mode='valid')
-    # Public goods effect.
-    # G for Goods
-    G = (C_conv > C_th)
-#    print "G.shape", G.shape
-    # all cells for which the effect of goods is above threshold is True in G.
-    # M for Metabolism
-    cost_board = S_cost * S + R_cost * R + C_cost * C
-    M = G * (1 - benefit) * cost_board
-    # all false in G don't benefit from public goods (G^True flips values)
-    M += (G^True) *  cost_board
-    if M[competitor_1[0], competitor_1[1]] > M[competitor_2t[0], competitor_2t[1]]:
-        C[competitor_1[0], competitor_1[1]] = C[competitor_2t[0], competitor_2t[1]]
-        S[competitor_1[0], competitor_1[1]] = S[competitor_2t[0], competitor_2t[1]]
-        R[competitor_1[0], competitor_1[1]] = R[competitor_2t[0], competitor_2t[1]]
-    elif M[competitor_1[0], competitor_1[1]] == M[competitor_2t[0], competitor_2t[1]]:
-        nothing #print 'buga'
-    else:
-        C[competitor_2t[0], competitor_2t[1]] = C[competitor_1[0], competitor_1[1]]
-        S[competitor_2t[0], competitor_2t[1]] = S[competitor_1[0], competitor_1[1]]
-        R[competitor_2t[0], competitor_2t[1]] = R[competitor_1[0], competitor_1[1]]    
+    def imagify_data(self):
+        global image, S, R, C
+        ## package data
+        data = sp.empty((self.N, self.N, 3))
+        data[:, :, 0] = self.S
+        data[:, :, 1] = self.R
+        data[:, :, 2] = self.C
+        resized_data = (255*data).repeat(4, axis=0).repeat(4, axis=1)
+        image = pygame.surfarray.make_surface(resized_data)
 
-def mutate():
-    global C, R, S
-    if sp.random.random()>0.1:
-        coords = sp.random.randint(N, size=2)
-        B = [C, R, S][sp.random.randint(3)]
-        B[coords[0], coords[1]] = sp.random.randint(2)
-
-def diffuse():
-    global R, S, C
-    m, n = sp.random.randint(N, size=2)
-    m1, n1 = (m+1)%N, (n+1)%N
-    if sp.random.randint(2):
-        # Truth for clockwise
-        for board in [R, S, C]:
-            board[[m, m, m1, m1], [n, n1, n1, n]] = board[[m1, m, m, m1], [n, n, n1, n1]]
-    else:
-        for board in [R, S, C]:
-            board[[m, m, m1, m1], [n, n1, n1, n]] = board[[m, m1, m1, m], [n1, n1, n, n]]
-
-def sample_frequency():
-    global S, R, C
-    res = zeros(8, dtype='uint32')
-    for i in sp.arange(8):
-        res[i] = sum(bool8((not 1 & i) ^ S) +
-                     bool8((not 2 & i) ^ R) +
-                     bool8((not 4 & i) ^ C))
-    return res
-
-def sample_nhood():
-    global S, R, C
-    res = zeros((8, 8), dtype='uint32')
-    
-def sample():
-    global sample_count, samples_frequency, samples_nhood
-    if not step % steps_per_sample:
-        samples_frequency[sample_count] = sample_count
-        samples_nhood[sample_count] = sample_count
-        sample_count += 1
-            
-def mainstuff():
-    global while_count, step, data
-    #print "while_count", while_count
-    competition()
-    diffuse()
-    sample()
-    step += 1
-    while_count += 1
-    print step
-
-## process data
-
-def imagify_data():
-    global image, S, R, C
-    ## package data
-    data = sp.ndarray(shape=[N, N, 3])
-    data[:, :, 0] = S
-    data[:, :, 1] = R
-    data[:, :, 2] = C
-    resized_data = (255*data).repeat(4, axis=0).repeat(4, axis=1)
-    image = pygame.surfarray.make_surface(resized_data)
-
-## update display
-
-def update_display():
-    global image
-    screen.blit(image, (0, 0))
-    pygame.display.flip()
+### update display
+#
+#def update_display():
+#    self.image
+#    self.screen.blit(image, (0, 0))
+#    self.pygame.display.flip()
 
 #clock = pygame.time.Clock()
 
 # infinite loop
 
 while_count = 0
-
+#a = gos()
 while True:
-    mainstuff() # the main algorithm
+#    a.mainstuff() # the main algorithm
 #    imagify_data()
 #    update_display()
+    print while_count
+    while_count += 1
