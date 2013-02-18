0163e489b017359be30339403595baf5887bbec6
Yuval Langer <yuval.langer@gmail.com>
2013-02-18 15:47:40 +0200

    Golangifying the settings file's variable names

--- golang/src/strife/sequential/defaults.go
+++ golang/src/strife/sequential/defaults.go
@@ -1,25 +1,26 @@
 package sequential
 
-var Default_Parameters = Parameters{
-	Generations:                  10,
-	R_Init_Odds:                  0,
-	S_Init_Odds:                  0,
-	Signal_Threshold:             3,
-	Cooperation_Effect_Threshold: 3,
-	S_Radius:                     1,
-	PG_Radius:                    1,
-	Board_Size:                   10,
-	D:                            1,
-	Mut_Odds_R:                   1e-4,
-	Mut_Odds_S:                   1e-4,
-	Basal_Cost:                   100,
-	Cooperation_Cost:             30,
-	Signal_Cost:                  3,
-	Receptor_Cost:                1,
-	Public_Goods_Effect:          0.6}
+var DefaultParameters = Parameters{
+	Generations:                10,
+	RInitOdds:                  0,
+	SInitOdds:                  0,
+	SignalThreshold:            3,
+	CooperationEffectThreshold: 3,
+	SRadius:                    1,
+	PGRadius:                   1,
+	BoardSize:                  10,
+	D:                          1,
+	MutOddsR:                   1e-4,
+	MutOddsS:                   1e-4,
+	MutOddsG:                   1e-4,
+	BasalCost:                  100,
+	CooperationCost:            30,
+	SignalCost:                 3,
+	ReceptorCost:               1,
+	PublicGoodsEffect:          0.6}
 
-var Default_Settings = Settings{
-	Snapshots_sample_num:                5,
-	Frequencies_sample_num:              10,
-	Neighborhood_frequencies_sample_num: 15,
-	Data_filename:                       "model-data.dat"}
+var DefaultSettings = Settings{
+	SnapshotsSampleNum:               50,
+	FrequenciesSampleNum:             50,
+	NeighborhoodFrequenciesSampleNum: 50,
+	DataFilename:                     "model-data.dat"}
