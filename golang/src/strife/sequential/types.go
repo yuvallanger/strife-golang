c4b6e3ae8840aa253a5242bd411c04aa6b770d46
Yuval Langer <yuval.langer@gmail.com>
2013-02-18 15:58:20 +0200

    Golangifying the methods and attribute names. Also,
    adding some separation between Czaran and Avigdor models to decrease
    code duplicity. Each has a struct named CzaranModel and AvigdorModel.
    Each struct has an anonymous field of Model.

--- golang/src/strife/sequential/types.go
+++ golang/src/strife/sequential/types.go
@@ -4,37 +4,79 @@ import (
 	"fmt"
 	"math/rand"
 	"miscow"
+	"time"
 )
 
 var r4strain = [8]int{0, 1, 0, 1, 0, 1, 0, 1} // index is the strain. value is the receptor allele.
 var s4strain = [8]int{0, 0, 1, 1, 0, 0, 1, 1} // index is the strain. value is the signal allele.
 var g4strain = [8]int{0, 0, 0, 0, 1, 1, 1, 1} // index is the strain. value is the receptor allele.
 
-type Board_strain [][]int       // [rows][columns]. possible values: {0,1,2,3}. s0r0 - 0; s0r1 - 1; s1r0 - 2; s1r1 - 3
-type Board_signal_num [][][]int // [signal types][rows][columns]
-type Board_prod [][]bool        // [rows][columns]
-type Board_pg_num [][]int       // [rows][columns]
+type BoardStrain [][]int      // [rows][columns]. possible values: {0,1,2,3}. s0r0 - 0; s0r1 - 1; s1r0 - 2; s1r1 - 3
+type BoardSignalNum [][][]int // [signal types][rows][columns]
+type BoardProd [][]bool       // [rows][columns]
+type BoardPGNum [][]int       // [rows][columns]
+
+// TODO This interface isn't very helpfull
+type Simulation interface {
+	showtiming(time.Time, time.Duration)
+	getGenerationIdx() int
+	setGenerationIdx(int)
+	incGenerationIdx()
+	setStartTime()
+	setSettings(Settings)
+	setParameters(Parameters)
+	getSettings() Settings
+	getParameters() Parameters
+	initBoards()
+	initBoardStrain()
+	initDataSamples()
+	sample()
+	save_json() error
+	SaveSnapshotsAsImages()
+	Fitness(Coordinate) float64
+	mutate(Coordinate) int
+	updateArrays(Coordinate, int, int)
+	Endgame(winnerCoord, loserCoord Coordinate)
+	Competition()
+	Diffuse(coord00 Coordinate, direction bool)
+}
+
+func (model *Model) getSettings() Settings {
+	return model.Settings
+}
+
+func (model *Model) setStartTime() {
+	model.StartTime = int64(time.Now().UnixNano())
+}
+
+type AvigdorModel struct {
+	Model
+}
+
+type CzaranModel struct {
+	Model
+}
 
 type Model struct {
-	Parameters       Parameters
-	Settings         Settings
-	Board_strain     Board_strain
-	Board_signal_num Board_signal_num
-	Board_prod       Board_prod
-	Board_pg_num     Board_pg_num
-	Data_samples     struct {
-		Snapshots             []Snapshot
-		Frequencies           []Frequency
-		Neighbors_frequencies []Neighbors_frequency
+	Parameters     Parameters
+	Settings       Settings
+	BoardStrain    BoardStrain
+	BoardSignalNum BoardSignalNum
+	BoardProd      BoardProd
+	BoardPGNum     BoardPGNum
+	DataSamples    struct {
+		Snapshots            []Snapshot
+		Frequencies          []Frequency
+		NeighborsFrequencies []NeighborsFrequency
 	}
-	Generation_i int
+	GenerationIdx int
 	//RandomState      rand.Rand
-	Start_Time string // start time in unix nanoseconds
+	StartTime int64 // start time in unix nanoseconds
 }
 
 type Snapshot struct {
 	Generation int
-	Data       Board_strain
+	Data       BoardStrain
 }
 
 type Snapshots []Snapshot
@@ -48,39 +90,41 @@ type Frequencies struct {
 	Frequency []Frequency
 }
 
-type Neighbors_frequency struct {
+type NeighborsFrequency struct {
 	Generation int
 	Data       [][]int
 }
 
-type Neighbors_frequencies struct {
-	Neighbors_frequency []Neighbors_frequency
+type NeighborsFrequencies struct {
+	NeighborsFrequency []NeighborsFrequency
 }
 
 type Parameters struct {
-	Generations                  int     // TODO
-	R_Init_Odds                  float64 // TODO
-	S_Init_Odds                  float64 // TODO
-	Signal_Threshold             int     // TODO
-	Cooperation_Effect_Threshold int     // TODO
-	S_Radius                     int     // the moor radius of signal production
-	PG_Radius                    int     // the moor radius of public goods production
-	Board_Size                   int     // a board will be sized Board_Size^2
-	D                            float64 // diffusion coefficient
-	Mut_Odds_R                   float64 // TODO
-	Mut_Odds_S                   float64 // TODO
-	Basal_Cost                   float64 // TODO
-	Cooperation_Cost             float64 // TODO
-	Signal_Cost                  float64 // TODO
-	Receptor_Cost                float64 // TODO
-	Public_Goods_Effect          float64 // TODO
+	Generations                int     // TODO
+	RInitOdds                  float64 // TODO
+	SInitOdds                  float64 // TODO
+	GInitOdds                  float64 // TODO
+	SignalThreshold            int     // TODO
+	CooperationEffectThreshold int     // TODO
+	SRadius                    int     // the moor radius of signal production
+	PGRadius                   int     // the moor radius of public goods production
+	BoardSize                  int     // a board will be sized BoardSize^2
+	D                          float64 // diffusion coefficient
+	MutOddsR                   float64 // TODO
+	MutOddsS                   float64 // TODO
+	MutOddsG                   float64 // TODO
+	BasalCost                  float64 // TODO
+	CooperationCost            float64 // TODO
+	SignalCost                 float64 // TODO
+	ReceptorCost               float64 // TODO
+	PublicGoodsEffect          float64 // TODO
 }
 
 type Settings struct {
-	Data_filename                       string // TODO
-	Snapshots_sample_num                int    // TODO
-	Frequencies_sample_num              int    // TODO
-	Neighborhood_frequencies_sample_num int    // TODO
+	DataFilename                     string // TODO
+	SnapshotsSampleNum               int    // TODO
+	FrequenciesSampleNum             int    // TODO
+	NeighborhoodFrequenciesSampleNum int    // TODO
 }
 
 type Config struct {
@@ -96,24 +140,24 @@ type Coordinate struct {
 Returns the same coordinates in modulus the size of the board.
 Board is a square, not a rectangle, so we need only one size argument.
 */
-func (c *Coordinate) get_toroid_coordinates(board_size int) Coordinate {
-	return Coordinate{r: miscow.MyMod(c.r, board_size),
-		c: miscow.MyMod(c.c, board_size)}
+func (c *Coordinate) ToroidCoordinates(boardSize int) Coordinate {
+	return Coordinate{r: miscow.MyMod(c.r, boardSize),
+		c: miscow.MyMod(c.c, boardSize)}
 }
 
-func rand_coord(board_size int) Coordinate {
+func RandCoord(boardSize int) Coordinate {
 	return Coordinate{
-		r: rand.Intn(board_size),
-		c: rand.Intn(board_size)}
+		r: rand.Intn(boardSize),
+		c: rand.Intn(boardSize)}
 }
 
-func (board_strain Board_strain) String() (s string) {
+func (board BoardStrain) String() (s string) {
 	s = " "
-	for i := range board_strain {
+	for i := range board {
 		s += fmt.Sprintf("%2v", i)
 	}
 	s += "\n"
-	for i0, v0 := range board_strain {
+	for i0, v0 := range board {
 		s += fmt.Sprintf("%v: ", i0)
 		for _, v1 := range v0 {
 			s += fmt.Sprintf("%v ", v1)
@@ -124,8 +168,8 @@ func (board_strain Board_strain) String() (s string) {
 	return
 }
 
-func (board_signal_num Board_signal_num) String() (s string) {
-	for i0, v0 := range board_signal_num {
+func (board BoardSignalNum) String() (s string) {
+	for i0, v0 := range board {
 		s += fmt.Sprintf("strain: %2v\n", i0)
 		for i1, v1 := range v0 {
 			s += fmt.Sprintf("\n%v: ", i1)
@@ -139,9 +183,9 @@ func (board_signal_num Board_signal_num) String() (s string) {
 	return
 }
 
-func (board_prod Board_prod) String() (s string) {
-	for _, row_vals := range board_prod {
-		for _, prod := range row_vals {
+func (board BoardProd) String() (s string) {
+	for _, rowVals := range board {
+		for _, prod := range rowVals {
 			switch prod {
 			case true:
 				s += fmt.Sprintf("%v", 1)
@@ -154,88 +198,92 @@ func (board_prod Board_prod) String() (s string) {
 	return
 }
 
+// fmt uses this function to generatie a string out of Parameters typed variables.
+// TODO: must convert all names into underscore notation.
 func (p Parameters) String() (s string) {
 	s += fmt.Sprintln("Parameters:")
 	s += fmt.Sprintln("Generations")
 	s += fmt.Sprintln("    ", p.Generations)
 	s += fmt.Sprintln("RInitOdds")
-	s += fmt.Sprintln("    ", p.R_Init_Odds)
+	s += fmt.Sprintln("    ", p.RInitOdds)
 	s += fmt.Sprintln("SInitOdds")
-	s += fmt.Sprintln("    ", p.S_Init_Odds)
-	s += fmt.Sprintln("STh")
-	s += fmt.Sprintln("    ", p.Signal_Threshold)
+	s += fmt.Sprintln("    ", p.SInitOdds)
+	s += fmt.Sprintln("GInitOdds")
+	s += fmt.Sprintln("    ", p.GInitOdds)
+	s += fmt.Sprintln("SignalThreshold")
+	s += fmt.Sprintln("    ", p.SignalThreshold)
 	s += fmt.Sprintln("CooperationEffectThreshold")
-	s += fmt.Sprintln("    ", p.Cooperation_Effect_Threshold)
+	s += fmt.Sprintln("    ", p.CooperationEffectThreshold)
 	s += fmt.Sprintln("SRad")
-	s += fmt.Sprintln("    ", p.S_Radius)
+	s += fmt.Sprintln("    ", p.SRadius)
 	s += fmt.Sprintln("CRad")
-	s += fmt.Sprintln("    ", p.PG_Radius)
+	s += fmt.Sprintln("    ", p.PGRadius)
 	s += fmt.Sprintln("BoardSize")
-	s += fmt.Sprintln("    ", p.Board_Size)
+	s += fmt.Sprintln("    ", p.BoardSize)
 	s += fmt.Sprintln("D")
 	s += fmt.Sprintln("    ", p.D)
 	s += fmt.Sprintln("MutOddsR")
-	s += fmt.Sprintln("    ", p.Mut_Odds_R)
+	s += fmt.Sprintln("    ", p.MutOddsR)
 	s += fmt.Sprintln("MutOddsS")
-	s += fmt.Sprintln("    ", p.Mut_Odds_S)
+	s += fmt.Sprintln("    ", p.MutOddsS)
 	return
 }
 
 func (settings Settings) String() (s string) {
 	s += fmt.Sprintln("Settings:")
-	s += fmt.Sprintln("Data_Filename:")
-	s += fmt.Sprintln("   ", settings.Data_filename)
-	s += fmt.Sprintln("Snapshots_sample_num:")
-	s += fmt.Sprintln("   ", settings.Snapshots_sample_num)
-	s += fmt.Sprintln("Frequencies_sample_num:")
-	s += fmt.Sprintln("   ", settings.Frequencies_sample_num)
-	s += fmt.Sprintln("Neighborhood_Frequencies_sample_num:")
-	s += fmt.Sprintln("   ", settings.Neighborhood_frequencies_sample_num)
+	s += fmt.Sprintln("DataFilename:")
+	s += fmt.Sprintln("   ", settings.DataFilename)
+	s += fmt.Sprintln("SnapshotsSampleNum:")
+	s += fmt.Sprintln("   ", settings.SnapshotsSampleNum)
+	s += fmt.Sprintln("FrequenciesSampleNum:")
+	s += fmt.Sprintln("   ", settings.FrequenciesSampleNum)
+	s += fmt.Sprintln("NeighborhoodFrequenciesSampleNum:")
+	s += fmt.Sprintln("   ", settings.NeighborhoodFrequenciesSampleNum)
 	return
 }
 
 func (model *Model) String() (s string) {
 	s += fmt.Sprintf("model.params:\n%v\n", model.Parameters)
-	s += fmt.Sprintf("%v\n", model.Board_strain)
+	s += fmt.Sprintf("%v\n", model.BoardStrain)
 	return
 }
 
-func (model *Model) get_cell_strain(c Coordinate) int {
-	return (model.Board_strain)[c.r][c.c]
+func (model *Model) CellStrain(c Coordinate) int {
+	return (model.BoardStrain)[c.r][c.c]
 }
 
-func (model *Model) get_cell_prod(c Coordinate) bool {
-	return (model.Board_prod)[c.r][c.c]
+func (model *Model) CellProd(c Coordinate) bool {
+	return (model.BoardProd)[c.r][c.c]
 }
 
-func (model *Model) get_cell_signal_num(c Coordinate, signal_type int) int {
-	return (model.Board_signal_num)[signal_type][c.r][c.c]
+func (model *Model) CellSignalNum(c Coordinate, signalType int) int {
+	return (model.BoardSignalNum)[signalType][c.r][c.c]
 }
 
-func (model *Model) get_cell_pg_num(c Coordinate) int {
-	return model.Board_pg_num[c.r][c.c]
+func (model *Model) CellPGNum(c Coordinate) int {
+	return model.BoardPGNum[c.r][c.c]
 }
 
-func (model *Model) set_cell_strain(c Coordinate, val int) {
-	(model.Board_strain)[c.r][c.c] = val
+func (model *Model) SetCellStrain(c Coordinate, val int) {
+	(model.BoardStrain)[c.r][c.c] = val
 }
 
-func (model *Model) set_cell_prod(c Coordinate, val bool) {
-	(model.Board_prod)[c.r][c.c] = val
+func (model *Model) SetCellProd(c Coordinate, val bool) {
+	(model.BoardProd)[c.r][c.c] = val
 }
 
-func (model *Model) set_cell_signal_num(c Coordinate, signal_type, val int) {
-	(model.Board_signal_num)[signal_type][c.r][c.c] = val
+func (model *Model) setCellSignalNum(c Coordinate, signalType, val int) {
+	(model.BoardSignalNum)[signalType][c.r][c.c] = val
 }
 
-func (model *Model) set_cell_pg_num(c Coordinate, val int) {
-	(model.Board_pg_num)[c.r][c.c] = val
+func (model *Model) setCellPGNum(c Coordinate, val int) {
+	(model.BoardPGNum)[c.r][c.c] = val
 }
 
-func (model *Model) Add_To_Cell_PG_Num(c Coordinate, val int) {
-	(model.Board_pg_num)[c.r][c.c] += val
+func (model *Model) AddToCellPGNum(c Coordinate, val int) {
+	(model.BoardPGNum)[c.r][c.c] += val
 }
 
-func (model *Model) Add_To_Cell_Signal_Num(c Coordinate, signal_type, val int) {
-	model.Board_signal_num[signal_type][c.r][c.c] += val
+func (model *Model) AddToCellSignalNum(c Coordinate, signalType, val int) {
+	model.BoardSignalNum[signalType][c.r][c.c] += val
 }
