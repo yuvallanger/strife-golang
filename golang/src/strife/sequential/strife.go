8b27490e5a6263880d804397821e57a773ae7c10
Yuval Langer <yuval.langer@gmail.com>
2013-02-18 16:16:01 +0200

    Reducing code duplicity
    Moving general Diffuse() code out of AvigdorModel and CzaranModel and
    into the parent Model. From avigdor_strife.go and czaran_strife.go and
    into strife.go

--- golang/src/strife/sequential/strife.go
+++ golang/src/strife/sequential/strife.go
@@ -26,3 +26,41 @@ func RandNeighbor(coord Coordinate, board_size int) (coord2 Coordinate) {
 	}
 	return
 }
+
+// Rotate 2x2 cells, but do not update the boards. It is left for each model's Diffuse() function
+func (model *Model) Rotate90(coord00 Coordinate, direction bool) (coordinates [2][2]Coordinate, before, after [2][2]int) {
+	// We'll set the coordinates for the four cells we're rotating.
+	coordinates[0][0] = coord00
+	coordinates[1][1] = Coordinate{r: coordinates[0][0].r + 1, c: coordinates[0][0].c + 1}
+	coordinates[1][1] = coordinates[1][1].ToroidCoordinates(model.Parameters.BoardSize)
+	coordinates[0][1] = Coordinate{r: coordinates[0][0].r, c: coordinates[1][1].c}
+	coordinates[1][0] = Coordinate{r: coordinates[1][1].r, c: coordinates[0][0].c}
+
+	// Save the tetrades
+	before[0][0] = model.CellStrain(coordinates[0][0])
+	before[0][1] = model.CellStrain(coordinates[0][1])
+	before[1][1] = model.CellStrain(coordinates[1][1])
+	before[1][0] = model.CellStrain(coordinates[1][0])
+
+	if direction {
+		// true is clockwise
+		after[0][0] = before[1][0]
+		after[0][1] = before[0][0]
+		after[1][1] = before[0][1]
+		after[1][0] = before[1][1]
+	} else {
+		// false is anticlockwise
+		after[0][0] = before[0][1]
+		after[0][1] = before[1][1]
+		after[1][1] = before[1][0]
+		after[1][0] = before[0][0]
+	}
+
+	// Assign the rotated cells
+	model.SetCellStrain(coordinates[0][0], after[0][0])
+	model.SetCellStrain(coordinates[0][1], after[0][1])
+	model.SetCellStrain(coordinates[1][0], after[1][0])
+	model.SetCellStrain(coordinates[1][1], after[1][1])
+
+    return
+}
