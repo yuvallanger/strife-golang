748753501ada82c41dcb12d03a089c650cbd7776
Yuval Langer <yuval.langer@gmail.com>
2013-02-21 23:33:22 +0200

    Adding specific allele counting function and plotter

--- golang/src/strife/analysis/analyzer.py
+++ golang/src/strife/analysis/analyzer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import numpy as np
-import pylab as pl
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
 import json
 import sys
 import PIL
@@ -23,38 +25,83 @@ legend = ['rsc','Rsc','rSc','RSc','rsC','RsC','rSC','RSC']
 
 def output_frequencies_samples(output_filename_prefix, frequencies_samples):
     print 'Starting working on frequencies samples'
-    pl.plt.clf()
-    pl.plt.hold(True)
+    plt.clf()
+    plt.hold(True)
     for strain in range(8):
         print 'plotting strain:', legend[strain]
-        pl.plt.plot(frequencies_samples['generation'], frequencies_samples['data'][:,strain])
-    pl.plt.ylabel('# cells of strain')
-    pl.plt.xlabel('generation #')
-    pl.plt.legend(legend)
+        plt.plot(frequencies_samples['generation'], frequencies_samples['data'][:,strain])
+    plt.ylabel('# cells of strain')
+    plt.xlabel('generation #')
+    plt.legend(legend)
     output_filename = output_filename_prefix+'-frequencies.png'
     print 'Saving figure:', output_filename
-    pl.plt.savefig(output_filename)
-    pl.plt.hold(False)
+    plt.savefig(output_filename)
+    plt.hold(False)
+
+def get_allele_frequencies(frequencies_samples):
+    r_freqs = frequencies_samples['data'][:,1] + \
+              frequencies_samples['data'][:,3] + \
+              frequencies_samples['data'][:,5] + \
+              frequencies_samples['data'][:,7]
+               
+    s_freqs = frequencies_samples['data'][:,2] + \
+              frequencies_samples['data'][:,3] + \
+              frequencies_samples['data'][:,6] + \
+              frequencies_samples['data'][:,7]
+
+    g_freqs = frequencies_samples['data'][:,4] + \
+              frequencies_samples['data'][:,5] + \
+              frequencies_samples['data'][:,6] + \
+              frequencies_samples['data'][:,7]
+
+    allele_freqs = np.empty((frequencies_samples['data'].shape[0], 3), dtype='int64')
+
+    allele_freqs[:,0] = r_freqs
+    allele_freqs[:,1] = s_freqs
+    allele_freqs[:,2] = g_freqs
+
+    return allele_freqs
+
+def output_allele_frequencies(output_filename_prefix, frequencies_samples):
+    print "Starting working on allele frequencies"
+    allele_freqs = get_allele_frequencies(frequencies_samples)
+    plt.hold(False)
+    plt.clf()
+    plt.hold(True)
+    
+    for gene in range(3):
+        print "Plotting", gene_legend[gene]
+        plt.plot(frequencies_samples['generation'], allele_freqs[:,gene])
+    plt.ylabel('# cells with functioning')
+    plt.xlabel('generation #')
+    plt.legend(gene_legend)
+    output_filename = output_filename_prefix+'-allele-frequencies.png'
+    print 'Saving image', output_filename
+    plt.savefig(output_filename)
+    plt.hold(False)
+    plt.clf()
+
+gene_legend = ['R', 'S', 'C']
 
 def output_neighbors_frequencies_samples(output_filename_prefix, neighbors_frequencies_samples):
     print 'Starting working on neighbors frequencies samples'
-    pl.plt.clf()
+    plt.clf()
     for center_strain in range(8):
         print 'Plotting neighbors of strain', legend[center_strain]
-        pl.plt.hold(True)
+        plt.hold(True)
         for neighbor_strain in range(8):
             print 'Plotting neighbor strain', legend[neighbor_strain]
             if center_strain != neighbor_strain:
-                pl.plt.plot(neighbors_frequencies_samples['generation'],
+                plt.plot(neighbors_frequencies_samples['generation'],
                             neighbors_frequencies_samples['data'][:,center_strain,neighbor_strain])
-        pl.plt.ylabel('# cells neighboring strain ' + legend[center_strain])
-        pl.plt.xlabel('generation #')
-        pl.plt.legend(legend)
+        plt.ylabel('# cells neighboring strain ' + legend[center_strain])
+        plt.xlabel('generation #')
+        plt.legend(legend)
         output_filename = output_filename_prefix+'-'+str(center_strain)+'-neighbors-frequencies.png'
         print 'Saving image', output_filename
-        pl.plt.savefig(output_filename)
-        pl.plt.hold(False)
-        pl.plt.clf()
+        plt.savefig(output_filename)
+        plt.hold(False)
+        plt.clf()
 
 def output_board_samples(output_filename_prefix, board_snapshot_samples):
     print 'Working on board images'
@@ -84,7 +131,9 @@ def main():
 
     frequencies_samples = numpyify_sample_sequence(data['DataSamples']['Frequencies'])
     neighbors_frequencies_samples = numpyify_sample_sequence(data['DataSamples']['NeighborsFrequencies'])
-    if frequencies_samples is not None: output_frequencies_samples(output_filename_prefix, frequencies_samples)
+    if frequencies_samples is not None:
+        output_frequencies_samples(output_filename_prefix, frequencies_samples)
+        output_allele_frequencies(output_filename_prefix, frequencies_samples)
     if neighbors_frequencies_samples is not None: output_neighbors_frequencies_samples(output_filename_prefix, neighbors_frequencies_samples)
     for n in sys.argv:
         if n == '--images':
